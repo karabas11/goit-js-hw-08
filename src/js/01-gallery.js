7220e7d4d6354bd89add5d9f7a77fd1893e49652
German Sokolovskyy <101786762+karabas11@users.noreply.github.com>
2023-08-04 18:41:16 +0300

    ended HW 08/01,03
    ended HW 08/01,03

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,31 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallerySmallImg = document.querySelector('.gallery');
+
+
+
+const markup = galleryItems.map(({preview, original, description}) =>
+`<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>
+</li>` ).join('');
+
+gallerySmallImg.insertAdjacentHTML('beforeend', markup);
+
+// gallerySmallImg.style.list-style = "none"
+
+console.log(SimpleLightbox);
+
+new SimpleLightbox('.gallery__item a', {
+  captionsData:'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  docClose: 'true'
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from "lodash.throttle";
 
+  const form = document.querySelector('.feedback-form');
+  const textarea = document.querySelector('textarea');
+  const email = document.querySelector('[type="email"]')
+
+  const formData = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+
+  form.addEventListener('submit', onFormSubmit);
+  form.addEventListener('input', throttle(onFormInput, 500));
+
+  delayedMessage();
+
+  function onFormSubmit(evt){ 
+  evt.preventDefault();
+
+  evt.target.reset();
+  localStorage.removeItem('feedback-form-state');
+  console.log(formData);
+};
+
+function onFormInput(evt){
+  formData.email = email.value;
+  formData.message = textarea.value;
+  console.log(email.value);
+  console.log(textarea.value);
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+
+function delayedMessage(){
+  const saveValue = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  if(saveValue){
+    email.value = saveValue.email || '';
+    textarea.value = saveValue.message || '';
+  }
+};
