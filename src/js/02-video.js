5bb136502f65b39c778ec2ed9ea9a22607416726
German Sokolovskyy <101786762+karabas11@users.noreply.github.com>
2023-08-04 18:26:34 +0300

    HW 08 qu2
    ended HW 08 qu2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Player from '@vimeo/player';
+import  throttle  from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+
+const player = new Vimeo.Player(iframe);
+
+let secondPlay = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+
+const onPlay = function(data) {
+  // data is an object containing properties specific to that event
+  localStorage.setItem("videoplayer-current-time", data.seconds);
+};
+
+player.setCurrentTime(secondPlay).then(function(seconds) {
+  // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+  switch (error.name) {
+      case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+      default:
+          // some other error occurred
+          break;
+  }
+});
+
+player.on('pause', throttle(onPlay, 1000));
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
